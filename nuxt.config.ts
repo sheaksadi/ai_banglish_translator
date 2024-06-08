d503421e9bf394b1ca3ebad7d56645bc97ada3ab
sheaksadi <sheaksadi123456@gmail.com>
2024-06-09 02:05:03 +0600

    idk

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     css: ['~/assets/css/main.css'],
+
     postcss: {
         plugins: {
             tailwindcss: {},
@@ -8,4 +9,5 @@ export default defineNuxtConfig({
         },
     },
 
+    modules: ["nuxt-security"]
 })
