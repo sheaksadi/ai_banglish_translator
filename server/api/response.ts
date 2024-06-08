9a57695c86b13299ef5c5c328ced20f2e777e657
sheaksadi <sheaksadi123456@gmail.com>
2024-06-09 02:09:33 +0600

    idk

--- server/api/response.ts
+++ server/api/response.ts
@@ -9,6 +9,15 @@ const openai = new OpenAIApi(configuration);
 
 
 export default defineEventHandler(async (event) => {
+    const headers = {
+        'Access-Control-Allow-Origin': 'Same-Origin',
+        'crossOriginResourcePolicy': 'same-origin',
+        'crossOriginOpenerPolicy': 'same-origin',
+        'crossOriginEmbedderPolicy': 'require-corp',
+        'contentSecurityPolicy': "default-src 'self';base-uri 'self';font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
+        'X-XSS-Protection': 1
+    }
+    setHeaders(event, headers)
     const body = await readBody(event)
     const messages: ChatCompletionRequestMessage[] = body.messages
 
